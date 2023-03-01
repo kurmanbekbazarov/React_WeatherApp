715a2b24a806a5254baf6f4071b4a044d214200d
Kurmanbek Bazarov <93934476+DrNyktersten@users.noreply.github.com>
2023-02-28 23:03:24 -0600

    Finished!

---
+++ src/api.js
@@ -0,0 +1,12 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '0c5953dd36msh53aac97883071edp159e21jsndc2d018e7b9b',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo"; 
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5"; 
+export const WEATHER_API_KEY = "8b1396028bd92cc284cf97b48f993514"; 
